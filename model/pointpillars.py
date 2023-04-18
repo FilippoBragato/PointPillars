caba843f3752b8021d862b938f4aacd590299afc
Filippo Bragato <filo.bragato@gmail.com>
2023-04-18 18:36:59 +0200

    Profiler #3

--- model/pointpillars.py
+++ model/pointpillars.py
@@ -220,7 +220,7 @@ class PointPillars(nn.Module):
     def __init__(self,
                  nclasses=3, 
                  voxel_size=[0.16, 0.16, 4],
-                 point_cloud_range=[-100.0, -100.0, -1, 100.0, 100.0, 3] ,
+                 point_cloud_range=[-16.0, -16.0, -1, 16.0, 16.0, 3] ,
                  max_num_points=32,
                  max_voxels=(16000, 40000)):
         super().__init__()
