141669e6d7c5624bd599e53094e62ed17a9c1453
Filippo Bragato <filo.bragato@gmail.com>
2023-04-18 18:23:35 +0200

    Profiler #2

--- profiler.py
+++ profiler.py
@@ -2,4 +2,4 @@ import pstats
 from pstats import SortKey
 
 p = pstats.Stats('restats')
-p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats()
+p.strip_dirs().sort_stats(SortKey.TIME).print_stats()
