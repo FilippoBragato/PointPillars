95666168e98431cd6e40294935258a7905db1b5d
Filippo Bragato <filo.bragato@gmail.com>
2023-04-27 17:28:00 +0200

    edit test #9

--- test.py
+++ test.py
@@ -74,6 +74,7 @@ def main(args):
                     temp_dict["pred_bboxes"] = r["lidar_bboxes"].tolist()
                     temp_dict["pred_labels"] = r["labels"].tolist()
                     temp_dict["pred_scores"] = r["scores"].tolist()
+                    temp_dict["gt_bboxes"] = data_dict["batched_gt_bboxes"][j].tolist()
                     results.append(temp_dict)
                 except:
                     print(r)
