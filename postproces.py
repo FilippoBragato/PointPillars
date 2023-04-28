611c8924a7ec35737fd29c4b152b8ea2b88657b1
Filippo Bragato <filo.bragato@gmail.com>
2023-04-28 17:30:16 +0200

    add ap

--- postproces.py
+++ postproces.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+import torch
+from pytorch3d.ops import box3d_overlap
 
 def open_results(path):
     results = []
@@ -69,7 +71,6 @@ def change_format(prediction):
     new_gt = []
     new_pred = []
     
-
     for bbox in ground_truth:
         new_bb = convert_bounding_box(bbox)
         new_gt.append(new_bb)
@@ -82,3 +83,60 @@ def change_format(prediction):
     prediction['pred_bboxes'] = np.array(new_pred)
 
     return prediction
+
+
+def get_iou(prediction):
+    gt = prediction['gt_bboxes']
+    pred = prediction['pred_bboxes']
+    _, iou = box3d_overlap(torch.tensor(gt, dtype=torch.float32), torch.tensor(pred, dtype=torch.float32))
+    return iou
+
+def pascal_voc_n(precision, recall, n=11):
+    # add 0 and 1 to precision and recall
+    precision = np.concatenate([[1], precision, [0]])
+    recall = np.concatenate([[0], recall, [1]])
+    # compute the precision envelope
+    for i in range(len(precision) - 1, 0, -1):
+        precision[i - 1] = np.maximum(precision[i - 1], precision[i])
+    # compute mean precision at n recall positions
+    recall_step = 1.0 / (n - 1)
+    ap = 0
+    for t in np.arange(0, 1 + recall_step, recall_step):
+        recall_mask = recall >= t
+        recall_idx = np.where(recall_mask)[0]
+        if len(recall_idx) == 0:
+            p = 0
+        else:
+            p = np.max(precision[recall_idx])
+        ap = ap + p
+    ap = ap / n
+    return ap
+
+def compute_average_precision(predictions, threshold=0.5):
+    # compute average precision for each class
+    aps = np.empty((3, len(predictions)))
+    for class_id in range(3):
+        for index_prediction, prediction in enumerate(predictions):
+            # get all predictions for the class
+            gt = prediction['gt_bboxes'][prediction['gt_labels'] == class_id]
+            pred = prediction['pred_bboxes'][prediction['pred_labels'] == class_id]
+            # compute iou
+            _, iou = box3d_overlap(torch.tensor(pred, dtype=torch.float32), torch.tensor(gt, dtype=torch.float32))
+            # compute average precision
+            true_positive = np.max(iou, axis = 1) > threshold
+
+            # sort by confidence
+            conf = prediction['pred_scores'][prediction['pred_labels'] == class_id]
+            sorted_conf = np.argsort(conf)[::-1]
+            true_positive = true_positive[sorted_conf]
+
+            # compute precision and recall
+            true_positive = np.cumsum(true_positive)
+            precision = true_positive / np.arange(1, len(true_positive) + 1)
+            recall = true_positive / gt.shape[0]
+
+            # compute average precision
+            ap = pascal_voc_n(precision, recall)
+            aps[class_id, index_prediction] = ap
+    return aps
+    
